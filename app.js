9e6dda2977c1fa5547e03e807e77e496ed164d7b
Sergio Balero <baleroeliel@gmail.com>
2023-07-19 13:08:02 -0300

    correccion en el archivo home.html y creacion del archivo style.css

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 const PUERTO = 3000;
 
-app.listen(PUERTO, ()=>console.log(`Server lenvantado: PUERTO: ${PUERTO}`));
+app.listen(PUERTO, ()=>console.log(`Server lenvantado: PUERTO ${PUERTO}`));
 
 app.get('/', function(req,res){
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
